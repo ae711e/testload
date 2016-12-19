555ad83a8c60fdd4ab65cdc90bd5cf62fc91f450
ae711e <ae711e@gmail.com>
2016-12-19 16:57:33 +0300

    очередное изменение

--- src/ae711/Main.java
+++ src/ae711/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
       // write your code here
       new lr().setVisible(true);
-      System.out.println("Output to console");
+      System.out.println("Output to console.");
     }
     
 }
